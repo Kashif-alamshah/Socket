1feb043c00133ae97c7c68ab918a75df95694c9f
Kashif Alam Shah <alamkashif827@gmail.com>
2024-12-12 14:02:26 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+namespace Clientside
+{
+    public partial class Form1 : Form
+    {
+        TcpClient Client = null;
+        NetworkStream ns;
+        StreamWriter streamWriter;
+        StreamReader streamReader;
+        private void ListenForMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    // Read the server's response (non-blocking)
+                    string response = streamReader.ReadLine();
+                    if (response != null)
+                    {
+                        // Update the TextBox on the UI thread with the server's message
+                        Invoke(new Action(() =>
+                        {
+                            richTextBox1.Text += Environment.NewLine + response;
+                        }));
+                    }
+                    else
+                    {
+                        // If the connection is closed (null response), break the loop
+                        break;
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Error: Server disconnected. " + e.Message);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //Client made
+            Client = new TcpClient("127.0.0.1",8888);
+            ns = Client.GetStream();
+            //Read Stream
+            streamReader = new StreamReader(ns);
+            richTextBox1.Text = streamReader.ReadLine();
+            streamWriter = new StreamWriter(ns);
+            streamWriter.Flush();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                try
+                {
+                    richTextBox1.Text += Environment.NewLine + "client >> " + textBox2.Text;
+                    streamWriter.WriteLine(textBox2.Text);
+                    textBox2.Clear();
+                }
+                catch(IOException ex)
+                {
+                    MessageBox.Show("Error Message: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,53 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Net.Sockets;
-using System.IO;
-namespace server
+using System.Windows.Forms;
+
+namespace Clientside
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            //Server created and started
-            TcpListener server = new TcpListener(System.Net.IPAddress.Any,8888);
-            server.Start();
-            Console.WriteLine("Server Started");
-            //Socket
-            Socket clientdoor = server.AcceptSocket();
-            //If client is connected
-            if (clientdoor.Connected)
-            {
-                //Create network stream (middle man) for read and write function
-                NetworkStream ns = new NetworkStream(clientdoor);
-                //Using writer to send message to client from server
-                Console.WriteLine("Server >> Hello Client");
-                StreamWriter streamWriter = new StreamWriter(ns);
-                StreamReader streamReader = new StreamReader(ns);
-                streamWriter.WriteLine("server >> I am Iron-man");
-                //Print everything which is in writer stream
-                streamWriter.Flush();
-                while (true)
-                {
-                    string clientmessage = streamReader.ReadLine();
-                    if (clientmessage != null)
-                    {
-                        Console.WriteLine("Client >> " + clientmessage);
-                        Console.Write("Server:|");
-                        string message = Console.ReadLine();
-                        string botmessage = "Server >> " + message;
-                        streamWriter.WriteLine(botmessage);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Client Disconnected");
-                        break;
-                    }
-                }
-            }
-            //If client is not connected
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
